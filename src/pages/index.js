f23d61548aec35f2dda3a5b4cf58f3c97a8de729
Shabuktagin Photon Khan <khan.photon@gmail.com>
2020-08-17 19:39:56 +0600

    feat: add a rotation human with phone

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,10 +4,13 @@ import { graphql, useStaticQuery } from 'gatsby'
 import { Layout } from 'Components/Layout'
 import { CardCollection } from 'Components/Card/Collection'
 import { useDimension } from 'Hooks/UseDimension'
+import { useScroll } from 'Hooks/UseScroll'
 import PhoneSvg from 'Svg/Phone/Phone.svg'
+import { HumanSvg } from 'Svg/Human'
 
 export default function Home() {
   const { width } = useDimension()
+  const scroll = useScroll()
 
   const imageWidth = (20 / 100) * width
   const imageHeight = (812 / 375) * imageWidth
@@ -44,17 +47,29 @@ export default function Home() {
           width >= 1440 && (
             <div
               style={{
+                perspective: 400,
                 position: `fixed`,
                 top: 120,
                 left: `50%`,
-                transform: `translateX(-50%)`
-              }}
-              className="phone-svg-container">
-              <img
-                alt={''}
-                src={PhoneSvg}
-                width={imageWidth}
-                height={imageHeight}/>
+                transform: `translateX(-50%) rotateY(${scroll * 3}deg)`
+              }}>
+              <div className="phone-svg-container">
+                <img
+                  alt={''}
+                  src={PhoneSvg}
+                  width={imageWidth}
+                  height={imageHeight} />
+              </div>
+              <div
+                style={{
+                  position: `absolute`,
+                  top: 0,
+                  left: `50%`,
+                  transform: `translateX(-50%)`
+                }}
+                className="human-svg-container">
+                <HumanSvg />
+              </div>
             </div>
           )
         }
